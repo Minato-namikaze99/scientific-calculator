59179bd75338382c99958fef4651dcb60488d64b
Indrajit Mandal <im.indrajit9954@gmail.com>
2023-04-14 23:22:20 +0530

    removed the edit in menubar

--- sc_calc.py
+++ sc_calc.py
@@ -288,21 +288,14 @@ def Standard():
  
 menubar = Menu(calc)
  
-# ManuBar 1 :
+# MenuBar 1 :
 filemenu = Menu(menubar, tearoff = 0)
 menubar.add_cascade(label = 'File', menu = filemenu)
 filemenu.add_command(label = "Standard", command = Standard)
 filemenu.add_command(label = "Scientific", command = Scientific)
 filemenu.add_separator()
 filemenu.add_command(label = "Exit", command = iExit)
- 
-# ManuBar 2 :
-# editmenu = Menu(menubar, tearoff = 0)
-# menubar.add_cascade(label = 'Edit', menu = editmenu)
-# editmenu.add_command(label = "Cut")
-# editmenu.add_command(label = "Copy")
-# editmenu.add_separator()
-# editmenu.add_command(label = "Paste")
+
  
 root.config(menu=menubar)
  
